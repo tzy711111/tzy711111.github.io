b03dc05aadf812280e3eee13ed632c809d0bd178
tzy711111 <103747988+tzy711111@users.noreply.github.com>
2022-11-28 14:36:22 +0800

    Add files via upload

---
+++ JavaDoc/member-search-index.js
@@ -0,0 +1,23 @@
+memberSearchIndex = [{"p": "my.uum", "c": "App", "l": "App()", "u": "%3Cinit%3E()"}, {
+    "p": "my.uum",
+    "c": "a",
+    "l": "disp_a()"
+}, {"p": "my.uum", "c": "b", "l": "disp_a()"}, {"p": "my.uum", "c": "c", "l": "disp_e()"}, {
+    "p": "my.uum",
+    "c": "a",
+    "l": "get_a()"
+}, {"p": "my.uum", "c": "b", "l": "get_b()"}, {"p": "my.uum", "c": "b", "l": "get_c()"}, {
+    "p": "my.uum",
+    "c": "c",
+    "l": "get_d()"
+}, {"p": "my.uum", "c": "c", "l": "get_e()"}, {
+    "p": "my.uum",
+    "c": "App",
+    "l": "main(String[])",
+    "u": "main(java.lang.String[])"
+}, {"p": "my.uum", "c": "b", "l": "sum()"}, {"p": "my.uum", "c": "c", "l": "sum()"}, {
+    "p": "my.uum",
+    "c": "c",
+    "l": "sum1()"
+}];
+updateSearchResults();
---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,5 @@
+typeSearchIndex = [{"p": "my.uum", "l": "a"}, {
+    "l": "All Classes and Interfaces",
+    "u": "allclasses-index.html"
+}, {"p": "my.uum", "l": "App"}, {"p": "my.uum", "l": "b"}, {"p": "my.uum", "l": "c"}];
+updateSearchResults();
