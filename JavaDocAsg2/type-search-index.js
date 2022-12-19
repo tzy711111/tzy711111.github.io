0e7660677b5070e52b6e6f23308d9104cc552bc6
tzy711111 <103747988+tzy711111@users.noreply.github.com>
2022-12-19 18:32:17 +0800

    Add files via upload

---
+++ JavaDocAsg2/type-search-index.js
@@ -0,0 +1,5 @@
+typeSearchIndex = [{"l": "All Classes and Interfaces", "u": "allclasses-index.html"}, {
+    "p": "my.uum",
+    "l": "App"
+}, {"p": "my.uum", "l": "Connect"}, {"p": "my.uum", "l": "TelegramBot"}];
+updateSearchResults();
